ce222a19c1ba18b81662207baeb4bc83be7aa339
AdrianTodt <adriantodt.ms@gmail.com>
2022-09-04 22:33:21 -0300

    Added SEO optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,32 @@ export default class MyDocument extends Document<{ emotionStyleTags: JSX.Element
     return (
       <Html lang="en">
         <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1"/>
+          <link rel="manifest" href="/site.webmanifest?v=1"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#ff6600"/>
+          <link rel="shortcut icon" href="/favicon.ico?v=1"/>
+          <meta name="msapplication-TileColor" content="#da532c"/>
+          <meta name="theme-color" content="#ffffff"/>
+          <meta name="title" content="AdrianTodt"/>
+          <meta name="description"
+                content="I'm a back-end focused full-stack software engineer based in Brazil. I engineer solutions for back-end, applications, AWS and more."/>
+          <meta name="keywords" content="adrian, todt, personal, portfolio, projects"/>
+          <meta name="robots" content="index, follow"/>
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
+          <meta name="language" content="English"/>
+          <meta name="author" content="AdrianTodt"/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://metatags.io/"/>
+          <meta property="og:title" content="AdrianTodt"/>
+          <meta property="og:description"
+                content="I'm a back-end focused full-stack software engineer based in Brazil. I engineer solutions for back-end, applications, AWS and more."/>
+          <meta property="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:url" content="https://metatags.io/"/>
+          <meta property="twitter:title" content="AdrianTodt"/>
+          <meta property="twitter:description"
+                content="I'm a back-end focused full-stack software engineer based in Brazil. I engineer solutions for back-end, applications, AWS and more."/>
           {this.props.emotionStyleTags}
         </Head>
         <body>
